0d0ae8dda1c58bee88493bb45c75119bed463600
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2023-02-15 12:50:07 +0100

    Add: validation frontend on register form

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,13 +5,19 @@ import.meta.glob([
     '../img/**'
 ])
 
+
 const btnDeletes = document.querySelectorAll('.ms-btn');
 console.log(btnDeletes);
 
 
 const btnForm = document.getElementById('button-submit');
-
 const errorCheckbox = document.querySelector('.error-field');
+const vatInput = document.getElementById('VAT');
+const requiredInput = document.querySelectorAll('.ms_form');
+const passwordRegister = document.getElementById('password');
+const passwordConfirm = document.getElementById('password-confirm');
+
+
 
 
 // Sezione per la cancellazione di un prodotto
@@ -31,20 +37,99 @@ btnDeletes.forEach((btn) =>
     })
 );
 
-if(btnForm) {
+btnForm.addEventListener('click', (event)=> {
 
-    btnForm.addEventListener('click', (event)=> {
-        event.preventDefault();
-        
-        const checkedBox = document.querySelectorAll('input[type="checkbox"]:checked');
-        const checkArray = [...checkedBox];
-        if (checkArray.length > 0) {
-            errorCheckbox.innerHTML = "";
-            document.getElementById('register-form').submit();
-        } else {
-            errorCheckbox.innerHTML = `<span class="text-danger">Devi inserire tutti i campi obbligatori</span>`;
-            errorCheckbox.scrollIntoView();
-        }
-    });
-} 
+
+    event.preventDefault();
+    const vatValue = vatInput.value;
+    const checkedBox = document.querySelectorAll('input[type="checkbox"]:checked');
+    const checkArray = [...checkedBox];
+    const arrayReq = [...requiredInput];
+    const valuePassword = passwordRegister.value;
+    const valuePasswordConfrim = passwordConfirm.value;
     
+    // input password
+    if(!valuePassword && !valuePasswordConfrim){
+        passwordRegister.classList.add('is-invalid');
+        passwordConfirm.classList.add('is-invalid');
+        const divPassword = document.querySelector(`.${passwordRegister.getAttribute('data-error')}`);
+        divPassword.innerHTML = `<p class="text-danger">La password deve essere compilata</p>`;
+        document.querySelector(`.${passwordConfirm.getAttribute('data-error')}`).innerHTML = `<p class="text-danger">La conferma password deve essere compilata</p>`;
+    
+    } else if((valuePassword.length > 0 && valuePasswordConfrim.length > 0) && (valuePassword !== valuePasswordConfrim) ){
+        passwordRegister.classList.add('is-invalid');
+        passwordConfirm.classList.add('is-invalid');
+        document.querySelector(`.${passwordRegister.getAttribute('data-error')}`).innerHTML = '';
+        document.querySelector(`.${passwordConfirm.getAttribute('data-error')}`).innerHTML = '';
+        document.querySelector('.pass-match').innerHTML = `<p class="text-danger">Le password devono essere uguali</p>`;
+
+    } 
+    else if(valuePassword.length === 0){
+        passwordRegister.classList.add('is-invalid');
+        document.querySelector(`.${passwordRegister.getAttribute('data-error')}`).innerHTML = `<p class="text-danger">Le password deve essere compilata</p>`;
+        if(valuePasswordConfrim){
+            passwordConfirm.classList.remove('is-invalid');
+            document.querySelector(`.${passwordConfirm.getAttribute('data-error')}`).innerHTML = '';
+        }
+    } else if( valuePasswordConfrim.length === 0){
+        passwordConfirm.classList.add('is-invalid');
+        document.querySelector(`.${passwordConfirm.getAttribute('data-error')}`).innerHTML = `<p class="text-danger">Le conferma password deve essere compilata</p>`;
+        if(valuePassword){
+            passwordRegister.classList.remove('is-invalid');
+            document.querySelector(`.${passwordRegister.getAttribute('data-error')}`).innerHTML = '';
+        }
+
+    }else if((valuePassword.length > 0 && valuePasswordConfrim.length > 0) && (valuePassword === valuePasswordConfrim )) {
+
+        passwordRegister.classList.remove('is-invalid');
+        passwordConfirm.classList.remove('is-invalid');
+        document.querySelector(`.${passwordRegister.getAttribute('data-error')}`).innerHTML = '';
+        document.querySelector(`.${passwordConfirm.getAttribute('data-error')}`).innerHTML = '';
+        document.querySelector('.pass-match').innerHTML = '';
+        
+        const boolCheck = checkArray.length > 0
+        const boolInput = arrayReq.every(el=> el.value.length > 0)
+    
+        if(boolCheck && boolInput){
+            document.getElementById('register-form').submit();
+        }
+        console.log({boolCheck, boolInput});
+
+    }
+
+
+// input name username email address vat
+    
+    arrayReq.forEach(el => {
+        if(el.value.length === 0){
+            el.classList.add('is-invalid');
+            // el.insertAdjacentHTML('afterend', `<p class="invalid-feedback"> Questo campo deve essere compilato </p>`)
+            const nameDiv = el.getAttribute('data-error');
+            document.querySelector(`.${nameDiv}Error`).innerHTML = `<p class="text-danger">Il campo ${el.getAttribute('data-name')} deve essere compilato</p>`;
+            
+        } else{
+            el.classList.remove('is-invalid');
+            document.querySelector(`.${el.getAttribute('data-error')}Error`).innerHTML = '';
+        }
+    })
+
+
+// input VAT
+    if(vatInput.value.length === 0){
+        vatInput.classList.add('is-invalid');
+        console.log('invalid');
+        document.querySelector('.vatError').classList.add('invalid-feedback');
+        document.querySelector('.vatError').innerHTML = `<p class="text-danger">Devi inserire p.Iva</p>`;
+    }
+
+// input CHECKBOX
+    if (checkArray.length > 0) {
+        errorCheckbox.innerHTML = "";
+    } else {
+        errorCheckbox.innerHTML = `<span class="text-danger">Inserisci almeno una tipologia</span>`;
+        errorCheckbox.scrollIntoView();
+    }
+    
+});
+
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,132 +1,153 @@
+<style>
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+</style>
+
 @extends('layouts.app')
 
 @section('content')
     <div class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="error-field">
-                    @error('types')
-                        <span class="text-danger" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
+
                 <div class="card">
                     <div class="card-header">{{ __('Register') }}</div>
 
                     <div class="card-body">
-                        <form method="POST" id="register-form" enctype="multipart/form-data"
-                            action="{{ route('register') }}">
+                        <form method="POST" id="register-form" enctype="multipart/form-data" action="{{ route('register') }}">
                             @csrf
-
+                            {{-- name --}}
                             <div class="mb-4 row">
                                 <label for="name"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Nome attività*') }}</label>
-
                                 <div class="col-md-6">
-                                    <input id="name" type="text"
-                                        class="form-control @error('name') is-invalid @enderror" name="name"
+                                    <input data-name="nome attività" data-error="name" id="name" type="text"
+                                        class="form-control ms_form @error('name') is-invalid @enderror" name="name"
                                         value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                    @error('name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                    <div class="nameError text-danger">
+                                        @error('name')
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
                             </div>
 
+                            {{-- username --}}
                             <div class="mb-4 row">
                                 <label for="username"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Nome utente*') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="username" type="text"
-                                        class="form-control @error('username') is-invalid @enderror" name="username"
+                                    <input data-name="nome utente" data-error="username" id="username" type="text"
+                                        class="form-control ms_form @error('username') is-invalid @enderror" name="username"
                                         value="{{ old('username') }}" required autocomplete="username" autofocus>
 
-                                    @error('username')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                    <div class="usernameError text-danger">
+                                        @error('username')
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
                                 </div>
                             </div>
 
+                            {{-- email --}}
                             <div class="mb-4 row">
                                 <label for="email"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-mail*') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                    <input data-name="email" data-error="email" id="email" type="email"
+                                        class="form-control ms_form @error('email') is-invalid @enderror" name="email"
                                         value="{{ old('email') }}" required autocomplete="email">
 
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                    <div class="emailError text-danger">
+                                        @error('email')
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
                             </div>
 
+                            {{-- address --}}
                             <div class="mb-4 row">
                                 <label for="address"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo*') }}</label>
-
                                 <div class="col-md-6">
-                                    <input id="address" type="text"
-                                        class="form-control @error('address') is-invalid @enderror" name="address"
+                                    <input data-name="indirizzo" data-error="address" id="address" type="text"
+                                        class="form-control ms_form @error('address') is-invalid @enderror" name="address"
                                         value="{{ old('address') }}" required autocomplete="address" autofocus>
 
-                                    @error('address')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                    <div class="addressError text-danger">
+                                        @error('address')
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
                             </div>
 
+                            {{-- vat --}}
                             <div class="mb-4 row">
                                 <label for="VAT"
                                     class="col-md-4 col-form-label text-md-right">{{ __('P. IVA*') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="VAT" type="text"
-                                        class="form-control @error('VAT') is-invalid @enderror" name="VAT"
+                                    <input data-name="p.Iva" data-error="VAT" id="VAT" type="number" style=""
+                                        class="form-control ms_form @error('VAT') is-invalid @enderror" name="VAT"
                                         value="{{ old('VAT') }}" required autocomplete="VAT" autofocus>
 
-                                    @error('VAT')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                    <div class="vatError text-danger">
+                                        @error('VAT')
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
                                 </div>
                             </div>
 
+                            {{-- img --}}
                             <div class="mb-4 row">
                                 <label for="img"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Immagine') }}</label>
-
                                 <div class="col-md-6">
                                     <input id="img" type="file"
                                         class="form-control @error('img') is-invalid @enderror" name="img"
                                         value="{{ old('img') }}" autofocus>
 
                                     @error('img')
-                                        <span class="invalid-feedback" role="alert">
+                                        <span class="text-danger" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                                 </div>
                             </div>
 
+                            {{-- types --}}
                             <div class="mb-4 row d-flex">
                                 <span class="col-md-4 col-form-label text-md-right">Seleziona una o più tipologie*</span>
                                 <div class="form-check @error('types') is-invalid @enderror">
                                     @foreach ($types as $type)
                                         <div>
-
                                             <input class="form-check-input" name="types[]" type="checkbox"
                                                 value="{{ $type->id }}" @checked($errors->any() ? in_array($type->id, old('types', [])) : false)
                                                 id="type-{{ $type->id }}">
@@ -140,37 +161,60 @@
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror --}}
+                                    <div class="error-field">
+                                        @error('types')
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
 
                                 </div>
                             </div>
 
-
+                            {{-- password --}}
                             <div class="mb-4 row">
                                 <label for="password"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Password*') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password"
+                                    <input data-error="passwordError" id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="new-password">
 
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                    <div class="passwordError text-danger">
+                                        @error('password')
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
                             </div>
 
-
+                            {{-- password confirm --}}
                             <div class="mb-4 row">
                                 <label for="password-confirm"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password*') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                        name="password_confirmation" required autocomplete="new-password">
+                                    <input data-error="passwordConfirmError" id="password-confirm" type="password"
+                                        class="form-control" name="password_confirmation" required
+                                        autocomplete="new-password">
+
+                                    <div class="passwordConfirmError text-danger">
+                                        @error('password')
+                                            <span class="text-danger" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
+                            </div>
+
+                            {{-- check if pass == confirm pass --}}
+                            <div class="pass-match">
+
                             </div>
 
                             <div class="col-md-6 text-decoration-underline mb-4">
