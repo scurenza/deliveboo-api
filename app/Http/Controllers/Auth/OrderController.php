7d5c2297ec7783cdb2fed4208bccbd3dae716bbf
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2023-02-15 17:49:21 +0100

    Add: Order add on db

--- app/Http/Controllers/Auth/OrderController.php
+++ app/Http/Controllers/Auth/OrderController.php
@@ -36,56 +36,28 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        // $form_data = $request->validate([
-        //     'name' => ['required', 'string', 'max:255'],
-        //     'last_name' => ['required', 'string', 'max:255'],
-        //     'email' => ['required', 'string', 'max:255'],
-        //     'phone_number' => ['required', 'numeric', 'max:13'],
-        //     'address' => ['required', 'string', 'max:255']
-        // ]);
-
-        // if ($request->success === '1') {
-        //     $form_data['success'] = 1;
-        // } else {
-        //     $form_data['success'] = 0;
-        // }
-
-        // $new_order = new Order();
-        // $new_order->fill($form_data);
-        // $new_order->save();
         $json = $request->all();
-        // dd($json['products']);
-
-        $tot = 0;
-
-        foreach ($json['products'] as $product) {
-            $tot += $product['price'] * $product['quantity'];
-        }
-
-        $form_data = [
-            "name" => $json['name'],
-            "last_name" => "Pluto",
-            "email" => $json['email'],
-            "phone_number" => 12345678,
-            "address" => "Paperopoli",
-            "amount" => $tot,
-            "success" => 1,
-            "date" => "2023-02-14"
-        ];
+        $form_data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255'],
+            'phone_number' => ['required', 'numeric', 'max:9999999999999'],
+            'success' => ['required'],
+            'date' => ['required'],
+            'amount' => ['required'],
+            'address' => ['required', 'string', 'max:255']
+        ]);
 
         $order = Order::create($form_data);
         $products = $json['products'];
 
-
-
         foreach ($products as $product) {
-            // $finalList[] = [
-            //     $product['id'] => ['quantity' => $product['quantity']]
-            // ];
+            $finalList[] = [
+                $product['id'] => ['quantity' => $product['quantity']]
+            ];
 
             $order->products()->attach($product['id'], ['quantity' => $product['quantity']]);
         }
-
 
         return response()->json([
             'success' => true,
