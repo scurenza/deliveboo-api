30c8cccc45fc61f9569a3b29ad4b2b3620d4a542
scurenza <fedechiar@hotmail.it>
2023-02-16 16:55:03 +0100

    Add: Braintree

--- resources/views/auth/braintree.blade.php
+++ resources/views/auth/braintree.blade.php
@@ -6,6 +6,18 @@
         <div style="display: flex;justify-content: center;align-items: center; color: white">
             <a id="submit-button" class="btn btn-sm btn-success">Submit payment</a>
         </div>
-
+        <script>
+            var button = document.querySelector('#submit-button');
+            braintree.dropin.create({
+                authorization: '{{ $token }}',
+                container: '#dropin-container'
+            }, function(createErr, instance) {
+                button.addEventListener('click', function() {
+                    instance.requestPaymentMethod(function(err, payload) {
+                        // Submit payload.nonce to your server
+                    });
+                });
+            });
+        </script>
     </div>
 @endsection
