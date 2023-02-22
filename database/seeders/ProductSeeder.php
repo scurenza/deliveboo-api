368a4d102cf5d75f612e92950099c1562fad4790
scurenza <fedechiar@hotmail.it>
2023-02-22 16:27:42 +0100

    Fix Seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class ProductSeeder extends Seeder
 {
@@ -15,31 +16,34 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        $productList = [
-            [
-                'name' => 'Margherita',
-                'user_id' => 1,
-                'description' => 'Origano, sale, pomodoro, mozzarella, olio',
-                'price' => 5.99,
-                'img' => '',
-                'available' => 1
-            ],
-            [
-                'name' => 'Biancaneve',
-                'user_id' => 1,
-                'description' => 'sale, mozzarella, olio',
-                'price' => 4.50,
-                'img' => '',
-                'available' => 1
-            ]
-        ];
+        // $productList = [
+        //     [
+        //         'name' => 'Margherita',
+        //         'user_id' => 1,
+        //         'description' => 'Origano, sale, pomodoro, mozzarella, olio',
+        //         'price' => 5.99,
+        //         'img' => '',
+        //         'available' => 1
+        //     ],
+        //     [
+        //         'name' => 'Biancaneve',
+        //         'user_id' => 1,
+        //         'description' => 'sale, mozzarella, olio',
+        //         'price' => 4.50,
+        //         'img' => '',
+        //         'available' => 1
+        //     ]
+        // ];
+
+        $json = Storage::disk('local')->get('/json/menu.json');
+        $productList = json_decode($json, true);
+
         foreach ($productList as $product) {
             $new_product = new Product();
             $new_product->name = $product['name'];
             $new_product->user_id = $product['user_id'];
             $new_product->description = $product['description'];
             $new_product->price = $product['price'];
-            $new_product->img = $product['img'];
             $new_product->available = $product['available'];
             $new_product->save();
         }
